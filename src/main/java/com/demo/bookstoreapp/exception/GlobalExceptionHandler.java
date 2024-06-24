c10eb39c6d62d018583b5ce10b19b505e4de64f4
Atul Singh <cocatul11@gmail.com>
2024-06-24 15:57:02 +0530

    feat: Update GlobalExceptionHandler to handle MethodArgumentNotValidException
    This commit modifies the `GlobalExceptionHandler` class to handle the `MethodArgumentNotValidException` exception. It overrides the `handleMethodArgumentNotValid` method to extract and format the validation errors from the exception and include them in the error response. This ensures that validation errors are properly handled and returned to the client.

--- src/main/java/com/demo/bookstoreapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/demo/bookstoreapp/exception/GlobalExceptionHandler.java
@@ -1,13 +1,22 @@
 package com.demo.bookstoreapp.exception;
 
 import com.demo.bookstoreapp.response.ErrorResponseDTO;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Collections;
+import java.util.List;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler({
       BookWithIdNotFoundException.class,
@@ -15,7 +24,7 @@ public class GlobalExceptionHandler {
   public ResponseEntity<ErrorResponseDTO> handleBookNotFoundException ( Exception e ) {
     return ResponseEntity
         .status(HttpStatus.NOT_FOUND)
-        .body(buildErrorResponse(HttpStatus.NOT_FOUND, e.getMessage()));
+        .body(buildErrorResponse(HttpStatus.NOT_FOUND, Collections.singletonList(e.getMessage())));
   }
 
   @ExceptionHandler({
@@ -24,7 +33,7 @@ public class GlobalExceptionHandler {
   public ResponseEntity<ErrorResponseDTO> handleBookAlreadyExistsException ( Exception e ) {
     return ResponseEntity
         .status(HttpStatus.CONFLICT)
-        .body(buildErrorResponse(HttpStatus.CONFLICT, e.getMessage()));
+        .body(buildErrorResponse(HttpStatus.CONFLICT, Collections.singletonList(e.getMessage())));
   }
 
   @ExceptionHandler({
@@ -33,10 +42,27 @@ public class GlobalExceptionHandler {
   public ResponseEntity<ErrorResponseDTO> handleGenericException ( Exception e ) {
     return ResponseEntity
         .status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
+        .body(buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, Collections.singletonList(e.getMessage())));
   }
 
-  private ErrorResponseDTO buildErrorResponse(HttpStatus status, String message) {
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid (
+      MethodArgumentNotValidException e,
+      HttpHeaders headers,
+      HttpStatusCode statusCode,
+      WebRequest request
+  ) {
+    List< String > errors = e.getBindingResult()
+        .getFieldErrors()
+        .stream()
+        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+        .toList();
+    return ResponseEntity
+        .status(statusCode)
+        .body(buildErrorResponse((HttpStatus) statusCode, errors));
+  }
+
+  private ErrorResponseDTO buildErrorResponse(HttpStatus status, List<String> message) {
     return ErrorResponseDTO.builder()
         .status("error")
         .statusCode(status.value())
--- src/main/java/com/demo/bookstoreapp/response/ErrorResponseDTO.java
+++ src/main/java/com/demo/bookstoreapp/response/ErrorResponseDTO.java
@@ -3,12 +3,15 @@ package com.demo.bookstoreapp.response;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Builder
 public class ErrorResponseDTO {
 
   private String status;
   private int statusCode;
-  private String message;
+  private List<String> message;
 
 }
