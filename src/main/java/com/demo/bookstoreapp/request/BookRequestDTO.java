34c990d41a4e7d5e108fe741a81e268ae7db0047
Atul Singh <cocatul11@gmail.com>
2024-06-24 15:57:16 +0530

    refactor: Update validation constraints for BookRequestDTO
    This commit updates the validation constraints for the `BookRequestDTO` class. The `summary` field now requires a minimum of 50 characters instead of 1, and the `isbn` field requires a minimum of 10 characters instead of 1. These changes ensure that the book summary and ISBN are within the specified length limits.

--- src/main/java/com/demo/bookstoreapp/request/BookRequestDTO.java
+++ src/main/java/com/demo/bookstoreapp/request/BookRequestDTO.java
@@ -3,10 +3,8 @@ package com.demo.bookstoreapp.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
@@ -20,14 +18,14 @@ public class BookRequestDTO {
   @Size(min = 1, max = 30, message = "Author name must be between 1 and 30 characters")
   private String author;
 
-  @Size(min = 1, max = 1000, message = "Summary must be between 1 and 100 characters")
+  @Size(min = 50, max = 1000, message = "Summary must be between 50 and 1000 characters")
   private String summary;
 
   @NotNull(message = "Publish year is required")
   private int publishYear;
 
   @NotBlank(message = "ISBN is required")
-  @Size(min = 1, max = 13, message = "ISBN must be between 1 and 13 characters")
+  @Size(min = 10, max = 13, message = "ISBN must be between 10 and 13 characters")
   private String isbn;
 
 }
--- src/main/java/com/demo/bookstoreapp/response/BookResponseDTO.java
+++ src/main/java/com/demo/bookstoreapp/response/BookResponseDTO.java
@@ -3,6 +3,8 @@ package com.demo.bookstoreapp.response;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @Builder
 public class BookResponseDTO {
@@ -13,5 +15,7 @@ public class BookResponseDTO {
   private String summary;
   private int publishYear;
   private String isbn;
+  private Date createdDate;
+  private Date lastModifiedDate;
 
 }
