b2f711e45a026f4ffaa94be4df40cc29dff38091
Atul Singh <cocatul11@gmail.com>
2024-06-24 15:57:24 +0530

    ```text feat: Update BookService interface to return BookResponseDTO instead of Book
    This commit modifies the BookService interface to update the return types of the createBook, updateBook, and getBook methods to BookResponseDTO instead of Book. This change ensures that the API consistently returns the BookResponseDTO object, which includes additional information such as the book's ID.

--- src/main/java/com/demo/bookstoreapp/service/BookService.java
+++ src/main/java/com/demo/bookstoreapp/service/BookService.java
@@ -2,14 +2,15 @@ package com.demo.bookstoreapp.service;
 
 import com.demo.bookstoreapp.model.Book;
 import com.demo.bookstoreapp.request.BookRequestDTO;
+import com.demo.bookstoreapp.response.BookResponseDTO;
 import org.springframework.data.domain.Page;
 
 public interface BookService {
 
-  public Book createBook( BookRequestDTO book);
-  public Book updateBook( String id, BookRequestDTO book);
+  public BookResponseDTO createBook( BookRequestDTO book);
+  public BookResponseDTO updateBook( String id, BookRequestDTO book);
   public void deleteBook( String id);
-  public Book getBook( String id);
+  public BookResponseDTO getBook( String id);
   public Page<Book> getBooksByTitle ( String title, int pageNo, int pageSize, String sortBy, String order);
   public Page<Book> getBooksByIsbn ( String isbn, int pageNo, int pageSize, String sortBy, String order);
   public Page<Book> getAllBooks( int pageNo, int pageSize, String sortBy, String order);
--- src/main/java/com/demo/bookstoreapp/service/impl/BookServiceImpl.java
+++ src/main/java/com/demo/bookstoreapp/service/impl/BookServiceImpl.java
@@ -3,8 +3,9 @@ package com.demo.bookstoreapp.service.impl;
 import com.demo.bookstoreapp.exception.BookWithIdNotFoundException;
 import com.demo.bookstoreapp.exception.BookWithIsbnAlreadyExistsException;
 import com.demo.bookstoreapp.model.Book;
-import com.demo.bookstoreapp.repository.BookRepository;
+import com.demo.bookstoreapp.repository.mongo.MongoBookRepository;
 import com.demo.bookstoreapp.request.BookRequestDTO;
+import com.demo.bookstoreapp.response.BookResponseDTO;
 import com.demo.bookstoreapp.service.BookService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.BeanUtils;
 
 import java.util.Optional;
 
@@ -21,27 +23,27 @@ import java.util.Optional;
 public class BookServiceImpl implements BookService {
 
   @Autowired
-  private BookRepository repository;
+  private MongoBookRepository repository;
 
   @Override
-  public Book createBook(BookRequestDTO bookRequest) {
+  public BookResponseDTO createBook(BookRequestDTO bookRequest) {
     checkIsbnExists(bookRequest.getIsbn(), false);
 
     Book newBook = buildBook(bookRequest);
     repository.save(newBook);
     log.info("Book with ISBN {} created successfully", bookRequest.getIsbn());
-    return newBook;
+    return mapToDto(newBook);
   }
 
   @Override
-  public Book updateBook(String id, BookRequestDTO bookRequest) {
+  public BookResponseDTO updateBook(String id, BookRequestDTO bookRequest) {
     checkBookExistsById(id);
     checkIsbnExists(bookRequest.getIsbn(), true);
 
     Book updatedBook = buildBook(id, bookRequest);
     repository.save(updatedBook);
     log.info("Book with ID {} updated successfully", id);
-    return updatedBook;
+    return mapToDto(updatedBook);
   }
 
   @Override
@@ -52,9 +54,10 @@ public class BookServiceImpl implements BookService {
   }
 
   @Override
-  public Book getBook(String id) {
-    return repository.findById(id)
+  public BookResponseDTO getBook(String id) {
+    Book book = repository.findById(id)
         .orElseThrow(() -> new BookWithIdNotFoundException(id));
+    return mapToDto(book);
   }
 
   @Override
@@ -75,24 +78,15 @@ public class BookServiceImpl implements BookService {
   }
 
   private Book buildBook(BookRequestDTO bookRequest) {
-    return Book.builder()
-        .title(bookRequest.getTitle())
-        .author(bookRequest.getAuthor())
-        .summary(bookRequest.getSummary())
-        .publishYear(bookRequest.getPublishYear())
-        .isbn(bookRequest.getIsbn())
-        .build();
+    Book book = new Book();
+    BeanUtils.copyProperties(bookRequest, book);
+    return book;
   }
 
   private Book buildBook(String id, BookRequestDTO bookRequest) {
-    return Book.builder()
-        .id(id)
-        .title(bookRequest.getTitle())
-        .author(bookRequest.getAuthor())
-        .summary(bookRequest.getSummary())
-        .publishYear(bookRequest.getPublishYear())
-        .isbn(bookRequest.getIsbn())
-        .build();
+    Book book = buildBook(bookRequest);
+    book.setId(id);
+    return book;
   }
 
   private void checkIsbnExists(String isbn, boolean isUpdate) {
@@ -111,7 +105,7 @@ public class BookServiceImpl implements BookService {
   private Page<Book> getBooksByField(String fieldName, String value, int pageNo, int pageSize, String sortBy, String order) {
     Sort sort = Sort.by(Sort.Direction.fromString(order), sortBy);
     Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
-    Optional<Page<Book>> books = switch ( fieldName ) {
+    Optional<Page<Book>> books = switch (fieldName) {
       case "title" -> repository.findByTitleContainingIgnoreCase(value, pageable);
       case "isbn" -> repository.findByIsbnContainingIgnoreCase(value, pageable);
       default -> Optional.empty();
@@ -119,5 +113,11 @@ public class BookServiceImpl implements BookService {
 
     log.info("Books with {} {} found", fieldName, value);
     return books.orElse(Page.empty());
+  }
+
+  private BookResponseDTO mapToDto( Book book) {
+    BookResponseDTO dto = BookResponseDTO.builder().build();
+    BeanUtils.copyProperties(book, dto);
+    return dto;
   }
 }
