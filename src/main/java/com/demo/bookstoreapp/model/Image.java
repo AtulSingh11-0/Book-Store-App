fac1cfccf7d358cacea96c23c1087f881b6c59b4
Atul Singh <cocatul11@gmail.com>
2024-06-26 15:49:24 +0530

    feat: Add lazy loading for book image and update dependencies
    This commit adds lazy loading functionality for the book image on the index.html page. It improves the page load performance by only loading the image when requested. Additionally, it updates the project's dependencies, including the addition of the `cloudinary-http44` and `java-dotenv` dependencies.

--- src/main/java/com/demo/bookstoreapp/model/Image.java
+++ src/main/java/com/demo/bookstoreapp/model/Image.java
@@ -4,8 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
 
 @Data
 @Builder
@@ -16,8 +20,13 @@ public class Image {
 
   @Id
   private String id;
-  private String name;
-  private String type;
-  private byte[] data;
+  private String publicId;
+  private String url;
+
+  @CreatedDate
+  private Date createdDate;
+
+  @LastModifiedDate
+  private Date lastModifiedDate;
 
 }
--- src/main/java/com/demo/bookstoreapp/response/ImageDTO.java
+++ src/main/java/com/demo/bookstoreapp/response/ImageDTO.java
@@ -3,13 +3,16 @@ package com.demo.bookstoreapp.response;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @Builder
 public class ImageDTO {
 
   private String id;
-  private String name;
-  private String type;
-  private byte[] data;
+  private String publicId;
+  private String url;
+  private Date createdDate;
+  private Date lastModifiedDate;
 
 }
