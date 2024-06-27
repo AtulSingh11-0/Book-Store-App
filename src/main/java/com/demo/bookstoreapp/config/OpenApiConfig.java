55a5e52af83ac8756d893205f3bf0eb4ac0c6cbc
Atul Singh <cocatul11@gmail.com>
2024-06-27 22:11:06 +0530

    feat: Update dependencies and configurations for lazy loading book image
    This commit updates the dependencies in the pom.xml file to use version 2.5.0 of the springdoc-openapi-starter-webmvc-ui artifact. Additionally, it adds lazy loading functionality for the book image on the index.html page, improving page load performance. The project's OpenAPI configuration is also updated to include a title, description, and version for the Book Store API.

--- src/main/java/com/demo/bookstoreapp/config/OpenApiConfig.java
+++ src/main/java/com/demo/bookstoreapp/config/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package com.demo.bookstoreapp.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,4 +16,13 @@ public class OpenApiConfig {
         .pathsToMatch("/api/v1/books/**")
         .build();
   }
+
+  @Bean
+  public OpenAPI springShopOpenAPI() {
+    return new OpenAPI()
+        .info(new Info().title("Book Store API")
+            .description("Book Store API reference for developers")
+            .version("v0.0.1"));
+  }
+
 }
