3972680b9503c50b29c564e06d5d2e40fe8559f7
Atul Singh <cocatul11@gmail.com>
2024-06-28 10:16:05 +0530

    feat: Add index for title field in Book collection
    This commit adds an index for the "title" field in the Book collection. The index is created in the MongoConfig class using the MongoTemplate's indexOps method. This change improves the performance of queries that involve sorting or filtering by the book title.

--- src/main/java/com/demo/bookstoreapp/config/MongoConfig.java
+++ src/main/java/com/demo/bookstoreapp/config/MongoConfig.java
@@ -1,16 +1,31 @@
 package com.demo.bookstoreapp.config;
 
+import com.demo.bookstoreapp.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.Index;
+import org.springframework.data.mongodb.core.index.IndexOperations;
 
 @Configuration
 @EnableMongoAuditing
 public class MongoConfig {
 
+  @Autowired
+  private MongoTemplate mongoTemplate;
+
   @Bean
   public AuditorAware<String> auditorAware () {
     return new AuditWareConfig();
+  }
+
+  public MongoConfig( MongoTemplate mongoTemplate ) {
+    this.mongoTemplate = mongoTemplate;
+    IndexOperations indexOps = mongoTemplate.indexOps(Book.class);
+    indexOps.ensureIndex(new Index().on("title", Sort.Direction.ASC));
   }
 }
