a67c13c9a0568b3768868938920199595573ec61
Atul Singh <cocatul11@gmail.com>
2024-07-20 15:11:03 +0530

    feat: Add CrossOrigin annotation to BookStoreAppApplication
    This commit adds the @CrossOrigin annotation to the BookStoreAppApplication class. The @CrossOrigin annotation allows cross-origin requests from any origin to be accepted by the application. This change enables the application to handle requests from different domains, improving its accessibility and flexibility.

--- src/main/java/com/demo/bookstoreapp/BookStoreAppApplication.java
+++ src/main/java/com/demo/bookstoreapp/BookStoreAppApplication.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Slf4j
+@CrossOrigin
 @EnableWebMvc
 @EnableCaching
 @SpringBootApplication
