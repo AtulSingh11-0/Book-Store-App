acb7b41f85b58137825d49b4f20af9740ce4ec92
Atul Singh <cocatul11@gmail.com>
2024-07-20 15:11:36 +0530

    refactor: Update AuditWareConfig to use @NotNull annotation for getCurrentAuditor method
    This commit updates the AuditWareConfig class to use the @NotNull annotation for the getCurrentAuditor method. The @NotNull annotation ensures that the method does not return null, improving the reliability and type safety of the codebase.

--- src/main/java/com/demo/bookstoreapp/config/AuditWareConfig.java
+++ src/main/java/com/demo/bookstoreapp/config/AuditWareConfig.java
@@ -1,5 +1,6 @@
 package com.demo.bookstoreapp.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.AuditorAware;
 
 import java.util.Optional;
@@ -8,6 +9,7 @@ public class AuditWareConfig implements AuditorAware<String> {
 
   // Returning empty instead of user since we're tracking just dates not users
   @Override
+  @NotNull
   public Optional< String > getCurrentAuditor () {
     return Optional.empty();
   }
