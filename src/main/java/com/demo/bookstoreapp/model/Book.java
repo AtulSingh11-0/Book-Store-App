16dc2535fe6e48bcdf354399a92555b9ffddf2ae
Atul Singh <cocatul11@gmail.com>
2024-07-20 15:11:15 +0530

    refactor: Update Book model annotations for required fields
    This commit updates the annotations in the Book model class to use the `requiredMode` property instead of the deprecated `required` property. The `requiredMode` property is set to `Schema.RequiredMode.REQUIRED` for the `title`, `author`, `publishYear`, and `isbn` fields. This change ensures that these fields are marked as required in the OpenAPI documentation and improves the consistency and clarity of the codebase.

--- src/main/java/com/demo/bookstoreapp/model/Book.java
+++ src/main/java/com/demo/bookstoreapp/model/Book.java
@@ -26,12 +26,12 @@ public class Book {
 
   @NotBlank(message = "Title is required")
   @Size(min = 1, max = 50, message = "Title must be between 1 and 50 characters")
-  @Schema(description = "Title of the book", example = "The Great Gatsby", required = true, maxLength = 50)
+  @Schema(description = "Title of the book", example = "The Great Gatsby", requiredMode = Schema.RequiredMode.REQUIRED, maxLength = 50)
   private String title;
 
   @NotBlank(message = "Author is required")
   @Size(min = 1, max = 30, message = "Author name must be between 1 and 30 characters")
-  @Schema(description = "Author of the book", example = "F. Scott Fitzgerald", required = true, maxLength = 30)
+  @Schema(description = "Author of the book", example = "F. Scott Fitzgerald", requiredMode = Schema.RequiredMode.REQUIRED, maxLength = 30)
   private String author;
 
   @Size(min = 50, max = 1000, message = "Summary must be between 50 and 1000 characters")
@@ -39,12 +39,12 @@ public class Book {
   private String summary;
 
   @NotNull(message = "Publish year is required")
-  @Schema(description = "Year of publication", example = "1925", required = true)
+  @Schema(description = "Year of publication", example = "1925", requiredMode = Schema.RequiredMode.REQUIRED)
   private int publishYear;
 
   @NotBlank(message = "ISBN is required")
   @Size(min = 10, max = 13, message = "ISBN must be between 10 and 13 characters")
-  @Schema(description = "ISBN of the book", example = "9780743273565", required = true, minLength = 10, maxLength = 13)
+  @Schema(description = "ISBN of the book", example = "9780743273565", requiredMode = Schema.RequiredMode.REQUIRED, minLength = 10, maxLength = 13)
   private String isbn;
 
   @DBRef
