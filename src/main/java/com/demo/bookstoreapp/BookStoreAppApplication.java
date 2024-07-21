43830196f14ab64391d587869ee2840f672bcf87
Atul Singh <cocatul11@gmail.com>
2024-07-21 13:01:48 +0530

    updates cors configurations

--- src/main/java/com/demo/bookstoreapp/BookStoreAppApplication.java
+++ src/main/java/com/demo/bookstoreapp/BookStoreAppApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Slf4j
-@CrossOrigin
+//@CrossOrigin
 @EnableWebMvc
 @EnableCaching
 @SpringBootApplication
